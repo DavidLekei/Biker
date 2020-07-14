f99c1f6738f173bdaf06f7d579359a9b0cddd386
DavidLekei <DavidLekei@users.noreply.github.com>
2020-07-13 20:00:58 -0500

    BikerLocationManager now checks the apps location permissions, and requests permissions if necessary. Location is then successfully retrieved from the device.

--- app/src/main/java/com/biker/api/LocationAPI/BikerLocationManager.java
+++ app/src/main/java/com/biker/api/LocationAPI/BikerLocationManager.java
@@ -1,16 +1,29 @@
 package com.biker.api.LocationAPI;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class BikerLocationManager {
 
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private Context context;
 
     public BikerLocationManager(Context context){
+        this.context = context;
         this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
     }
 
@@ -19,6 +32,34 @@ public class BikerLocationManager {
 //    }
 
     public void getCurrentLocation(){
-        
+        checkPermission();
+
+        Task task = this.fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                System.out.println("Sucessfully retrieved the devices location!");
+                System.out.println("Location: " + location.toString());
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("Failed to retrieve device location.");
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this.context,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){//Can add more as per requirement
+
+            ActivityCompat.requestPermissions((Activity)this.context,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+        }
     }
 }
