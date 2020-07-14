e15fcdf02f09a88f9a4ffd7ffa7b587456728d10
DavidLekei <DavidLekei@users.noreply.github.com>
2020-07-13 20:06:20 -0500

    Removed unused import

--- app/src/main/java/com/biker/api/LocationAPI/BikerLocationManager.java
+++ app/src/main/java/com/biker/api/LocationAPI/BikerLocationManager.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
